92989fc48405a66bbe3cd6d7fd0982ae97527bd2
Taha <x3727349s@iespoblenou.org>
2016-10-11 17:46:14 +0200

    agegit el oncreatmenu

--- app/src/main/java/com/example/x3727349s/magic/MainActivityFragment.java
+++ app/src/main/java/com/example/x3727349s/magic/MainActivityFragment.java
@@ -3,10 +3,14 @@ package com.example.x3727349s.magic;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.zip.Inflater;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -24,6 +28,12 @@ public class MainActivityFragment extends Fragment {
     private int contadorVida2=20, contadorVeneno2=0;
 
     public MainActivityFragment() {
+    }
+    //esto es para inflar el fragment (lo hacemos para el boton reset)
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_main_fragment, menu);
     }
 
     @Override
